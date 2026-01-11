540bd8487c5bf20ea69836efdbc9a753a4e8683c
arFaisal043 <arfaisal463@gmail.com>
2026-01-12 01:30:38 +0600

    start revision OOP in js

--- OOP.js
+++ OOP.js
@@ -1,229 +1,82 @@
-// ................. OBJECT ....................
 /*
-let student = {
-    fullName: "Abdur Rahman Faisal",
-    age: 22,
-    marks: 90.5,
-    printMarks: function(){
-        console.log(`marks = ${this.marks}`);
-    }
+
+=> Object:
+-Object is an entity having state(properties -> variable) and behavior(method -> function)
+
+const user = {
+    name: "Faisal",
+    id: 1
 }
-console.log(student);
-console.log(student.printMarks())
-*/
+console.log(user);
+
+_______________________________________________________________________________________________
+
+=> Prototype:
+- In JavaScript, a prototype is a mechanism that allows objects to inherit properties and methods from another object.
+👉 JavaScript uses prototype-based inheritance, not class-based inheritance (like Java or C++).
 
 
-
-// ...................... PROTOTYPE ...........................
-
-/*
+// create object
 const employee = {
     calcTax() {
-        console.log(`tax rate is 10%`);
+        console.log("Tax rate is 10%");
     }
 }
+
 const employee1 = {
-    salary: 50000,
+    salary: 50000
 }
+
 const employee2 = {
-    salary: 60000,
-}
+  salary: 60000,
+};
+
 const employee3 = {
-    salary: 70000,
+  salary: 70000,
+  calcTax() {
+    console.log("Tax rate is 20% for employee 3");
+  },
+};
+
+// create prototype and connect object
+employee1.__proto__ = employee;  // now employee1 has a prototype named calcTax
+employee2.__proto__ = employee;  // now employee2 has a prototype named calcTax
+employee3.__proto__ = employee;  // now employee3 has a prototype named calcTax
+
+// check property
+employee1.calcTax(); // Tax rate is 10%
+employee2.calcTax(); // Tax rate is 10%
+employee3.calcTax(); // Tax rate is 20% -> own method priority is high
+*/
+
+// create object
+const employee = {
     calcTax() {
-        console.log(`tax rate is 15%`);
+        console.log("Tax rate is 10%");
     }
 }
 
-employee1.__proto__ = employee;
-employee2.__proto__ = employee;
-employee3.__proto__ = employee;
-
-console.log(employee1)
-console.log(employee2)
-console.log(employee3)
-*/
-
-
-
-// .................... Classes in js ..........................
-/*
-class structCar {
-    start() {
-        console.log("start");
-    }
-    stop() {
-        console.log("stop");
-    }
-    setBrand(brand) {
-        this.Brand = brand;
-    }
+const employee1 = {
+    salary: 50000
 }
 
-const tesla = new structCar();
-const bmw = new structCar();
-const lexus = new structCar();
+const employee2 = {
+  salary: 60000,
+};
 
-tesla.setBrand("TESLA");
-bmw.setBrand("BMW");
-lexus.setBrand("LEXUS");
+const employee3 = {
+  salary: 70000,
+  calcTax() {
+    console.log("Tax rate is 20% for employee 3");
+  },
+};
 
-console.log(tesla);
-console.log(bmw);
-console.log(lexus);
-*/
+// create prototype and connect object
+employee1.__proto__ = employee;  // now employee1 has a prototype named calcTax
+employee2.__proto__ = employee;  // now employee2 has a prototype named calcTax
+employee3.__proto__ = employee;  // now employee3 has a prototype named calcTax
 
-
-// ....................... CONSTRUCTOR ...........................
-
-/*
- *** A constructor is a special function that creates and initializes an object instance of a class. In JavaScript, a constructor gets called when an object is created using the new keyword. The purpose of a constructor is to create a new object and set values for any existing object properties. ***
-
-class structCar {
-    constructor(brand , milage) {
-        this.brand = brand;
-        this.milage = milage;
-        console.log("creating a new object");
-    }
-    start() {
-        console.log("start");
-    }
-    stop() {
-        console.log("stop");
-    }
-}
-
-const bmw = new structCar("bmw" , 10);
-const tesla = new structCar("tesla" , 20);
-console.log(bmw);
-console.log(tesla);
-*/
-
-
-
-
-// ...................... INHERITANCE .......................
-/*
-In programming, inheritance refers to passing down characteristics from a parent to a child so that a new piece of code can reuse and build upon the features of an existing one.
-
-
-class person {
-    constructor(species) {
-        this.species = "homo sapience";
-    }
-    eat() {
-        console.log("eat");
-    }
-    sleep() {
-        console.log("sleep");
-    }
-    work() {
-        console.log("DO Nothing");
-    }
-}
-
-class engineer extends person{
-    work() {
-        console.log("solve problems");
-    }
-}
-
-class doctor extends person {
-    work() {
-        console.log("Medical checkup");
-    }
-}
-
-const faisal = new engineer();
-const abc = new doctor();
-
-// console.log(person);
-
-// console.log(engineer);
-// console.log(doctor);
-
-console.log(faisal);
-console.log(abc);
-*/
-
-
-
-
-// ..................... STRONG KEYWORD ........................
-
-/*
-// Parent Class
-class parent {
-    hello1() {
-        console.log("Hello 1 Function");
-    }
-    hello2() {
-        console.log("Hello 2 Function");
-    }
-}
-
-// child class
-class child extends parent{
-    demo() {
-        super.hello1();  //invoked parent's method
-        super.hello2();  //invoked parent's method
-    }
-}
-
-const obj = new child();
-obj.demo();
-*/
-
-
-/* 
-Practice Question: 1
-You are creating a website for your university. create a class 'User' with 2 properties 'name' and 'email'. it also has a method called viewData() that allows user to view website data.
-
-
-class User {
-    constructor(name , email) {
-        this.name = name;
-        this.email = email;
-    }
-    viewData() {
-        console.log("This is Data........");
-    }
-}
-
-const student1 = new User("Abdur Rahman Faisal" , "arfaisal463@gmail.com");
-const student2 = new User("Israt Jahan" , "esha825@gmail.com");
-const student3 = new User("Jannatul Naima" , "nowshin123@gmail.com");
-console.log(student1);
-console.log(student2);
-console.log(student3);
-*/
-
-/*
-Practice Ques: 2
-Create a new Class called "Admin" which inherits from the "User". Add a new method called editData() to admin that allows it to edit website data.
-
-class User {
-    constructor(name , email) {
-        this.name = name;
-        this.email = email;
-    }
-    viewData() {
-        console.log("This is Data........");
-    }
-}
-
-class Admin extends User{
-    constructor(name , email) {
-        super(name , email);
-    }
-    editData() {
-        console.log("This data can be edited.........");
-    }
-}
-
-const admin1 = new Admin("admin" , "admina@gmail.com");
-console.log(admin1);
-*/
-
-
-
+// check property
+employee1.calcTax(); // Tax rate is 10%
+employee2.calcTax(); // Tax rate is 10%
+employee3.calcTax(); // Tax rate is 20% -> own method priority is high
